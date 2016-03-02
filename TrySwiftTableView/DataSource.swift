ca1ca69bd68bdca7c09764f9ef9b58cbf1ffbe84
naoto0n2 <onagi@awa.fm>
2016-03-02 22:44:01 +0900

    Take DataSource out of Table ViewController

---
+++ TrySwiftTableView/DataSource.swift
@@ -0,0 +1,59 @@
+//
+//  DataSource.swift
+//  TrySwiftTableView
+//
+//  Created by Naoto Onagi on 2016/03/02.
+//  Copyright © 2016 Naoto Onagi. All rights reserved.
+//
+
+import UIKit
+
+class DataSource: NSObject, UITableViewDataSource {
+    
+    private var album = Album()
+    
+    func addItemTo(tableView: UITableView) {
+        if self.album.numberOfTracks < 5 {
+            self.album = self.album.addNewTrackAtIndex(0)
+            self.insertTopRowIn(tableView)
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.album.numberOfTracks
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCellWithIdentifier("TrackCell", forIndexPath: indexPath) as? TrackCell else {
+            return UITableViewCell()
+        }
+        let track = self.album.trackAtPosition(indexPath.row)
+        cell.fillWith(track)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        self.album = self.album.moveTrack(sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        switch editingStyle {
+        case .Delete:
+            self.album = self.album.deleteTrackAtIndex(indexPath.row)
+            self.deleteRowAtIndexPath(indexPath, from: tableView)
+        default: break
+        }
+    }
+    
+    func insertTopRowIn(tableView: UITableView) {
+        tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: .Fade)
+    }
+    
+    func deleteRowAtIndexPath(indexPath: NSIndexPath, from tableView: UITableView) {
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    }
+}
--- TrySwiftTableView/ViewController.swift
+++ TrySwiftTableView/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    private var album = Album()
+    private var dataSource = DataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.dataSource = self.dataSource
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "addNewTrack:")
     }
@@ -23,19 +24,7 @@ class ViewController: UIViewController {
     // MARK: - Private methods
 
     dynamic private func addNewTrack(sender: UIBarButtonItem) {
-        if album.numberOfTracks < 5 {
-            self.album = self.album.addNewTrackAtIndex(0)
-            self.insertTopRow()
-        }
-    }
-    
-    private func insertTopRow() {
-        self.album.addNewTrackAtIndex(0)
-        self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: .Fade)
-    }
-    
-    private func deleteRowAtIndexPath(indexPath: NSIndexPath) {
-        self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        self.dataSource.addItemTo(self.tableView)
     }
 
     // MARK: - UIViewController override
@@ -44,40 +33,5 @@ class ViewController: UIViewController {
         super.setEditing(editing, animated: animated)
         
         self.tableView.setEditing(editing, animated: true)
-    }
-    
-}
-
-// MARK: - UITableViewDataSource
-
-extension ViewController: UITableViewDataSource {
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.album.numberOfTracks
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCellWithIdentifier("TrackCell", forIndexPath: indexPath) as? TrackCell else {
-            return UITableViewCell()
-        }
-        let track = self.album.trackAtPosition(indexPath.row)
-        cell.fillWith(track)
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
-        self.album = self.album.moveTrack(sourceIndexPath.row, toIndex: destinationIndexPath.row)
-    }
-    
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        switch editingStyle {
-        case .Delete:
-            self.album = self.album.deleteTrackAtIndex(indexPath.row)
-            self.deleteRowAtIndexPath(indexPath)
-        default: break
-        }
     }
 }
